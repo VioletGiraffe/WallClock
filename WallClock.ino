502e149070a9f8d00164787ecb669235ec030a5e
VioletGiraffe <alexxxx89@ya.ru>
2018-05-02 00:28:49 +0300

    FastLedDisplayPainter text test

--- WallClock.ino
+++ WallClock.ino
@@ -3,25 +3,13 @@
 #include <Painter_FastLED.hpp>
 
 constexpr auto DATA_PIN = 19;
-FastLedDisplayPainter<8, 8, DATA_PIN> painter;
 
 void setup()
 {
-	
-	// for (int dot = 0; dot < NUM_LEDS; ++dot)
-	// {
-	// 	hue[dot] = 240 * dot / NUM_LEDS;
-	// 	//leds[dot].setHSV(hue[dot], 255, 10);
-	// 	hue[dot] = CRGB::Black;
-	// 	FastLED.show();
-	// 	delay(20);
-	// }
+	FastLedDisplayPainter<8, 8, DATA_PIN> painter;
+	painter.print("6");
 }
 
 void loop()
 {
-	// hue[0] += 1;
-	// leds[0].setHSV(hue[0], 255, 255);
-	// FastLED.show();
-	// delay(20);
 }
