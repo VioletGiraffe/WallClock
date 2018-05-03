575d5af7caa4ca876a697eabcb042200f74578da
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2018-05-03 22:58:46 +0300

    Transposition test

--- WallClock.ino
+++ WallClock.ino
@@ -6,8 +6,11 @@ constexpr auto DATA_PIN = 19;
 
 void setup()
 {
-	FastLedDisplayPainter<8, 8, DATA_PIN> painter;
+	FastLedDisplayPainter<8*4, 8, DATA_PIN, true> painter;
+	painter.setTextColor(Color{3, 1, 1});
 	painter.print("6");
+	//for (uint16_t x = )
+	painter.setPixel(2, 9, Color{0, 2, 0});
 }
 
 void loop()
