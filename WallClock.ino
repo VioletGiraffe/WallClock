3dda53466d4afa7eae9b5ce274d5c660598ce812
VioletGiraffe <alexxxx89@ya.ru>
2018-08-25 21:21:21 +0300

    Arduino UNO and ESP32 compilation fixed

--- WallClock.ino
+++ WallClock.ino
@@ -9,8 +9,8 @@ void setup()
 	FastLedDisplayPainter<8*4, 8, DATA_PIN, true> painter;
 	painter.setTextColor(Color{3, 1, 1});
 	painter.print("6");
-	//for (uint16_t x = )
-	painter.setPixel(2, 9, Color{0, 2, 0});
+	delay(20);
+	painter.setPixel(31, 7, Color{0, 2, 0});
 }
 
 void loop()
